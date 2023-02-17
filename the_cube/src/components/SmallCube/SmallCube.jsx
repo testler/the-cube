20255de1c14ddbc560bdff7df4ad1341c764099f
testler <joshgarst@me.com>
2023-02-16 23:27:00 -0600

    cube animation

--- the_cube/src/components/SmallCube/SmallCube.jsx
+++ the_cube/src/components/SmallCube/SmallCube.jsx
@@ -1,38 +1,52 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useFrame } from '@react-three/fiber'
-
-
+import { useFrame, useThree } from '@react-three/fiber';
 
 const SmallCube = (props) => {
-
   const [x, y, z] = props.position;
   const [hovered, hover] = useState(false);
   const [position, setPosition] = useState(props.position);
+  const [multiplier, setMultiplier] = useState(1);
 
-    useFrame((state, delta)=>{
-    })
-
+  const { camera } = useThree();
 
   const handleMouseEnter = () => {
     hover(true);
-    
-  }
+  };
+
   const handleMouseLeave = () => {
     hover(false);
     setPosition(props.position);
-  }
-  useEffect(() => { }, []);
+  };
+
+  const handlePointerDown = (event) => {
+    event.stopPropagation();
+    camera.lookAt(x, y, z);
+  };
+
+  // Animate the cube
+  useFrame((state, delta) => {
+    // If the cube is hovered over, increase the multiplier up to 1.25
+    if (hovered) {
+      setMultiplier(Math.min(multiplier + delta * 0.4, 1.25));
+    } else {
+      setMultiplier(Math.max(multiplier - delta * 0.4, 1));
+    }
+
+    // Update the cube's position based on the multiplier
+    setPosition([x * multiplier, y * multiplier, z * multiplier]);
+  });
+
   return (
     <mesh
-    {...props}
+      {...props}
       position={position}
-      // scale={hovered ? 1.1 : 1}
-      onPointerOver={(event) => handleMouseEnter()}
-      onPointerOut={(event) => handleMouseLeave()}>
+      onPointerOver={handleMouseEnter}
+      onPointerOut={handleMouseLeave}
+      onPointerDown={handlePointerDown}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'purple' : 'blue'} />
+      <meshStandardMaterial color={hovered ? '#8b15f9' : '#1537f9'} />
     </mesh>
-  )
-}
+  );
+};
 
-export default SmallCube
+export default SmallCube;
